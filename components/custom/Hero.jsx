25f0cb6aef7750b279213a898b5aa299b1646772
SwayamJain8 <jainswyam4006@gmail.com>
2025-03-17 01:12:51 +0530

    Authentication Done

--- components/custom/Hero.jsx
+++ components/custom/Hero.jsx
@@ -1,16 +1,24 @@
 "use client";
 
 import { MessagesContext } from "@/context/MessagesContext";
+import { UserDetailContext } from "@/context/UserDetailContext";
 import Colors from "@/data/Colors";
 import Lookup from "@/data/Lookup";
 import { Link } from "lucide-react";
 import { ArrowRight } from "lucide-react";
 import React, { useContext, useState } from "react";
+import SignInDialog from "./SignInDialog";
 
 const Hero = () => {
   const [userInput, setUserInput] = useState();
   const { messages, setMessages } = useContext(MessagesContext);
+  const { userDetail, setUserDetail } = useContext(UserDetailContext);
+  const [openDialog, setOpenDialog] = useState(false);
   const onGenerate = (input) => {
+    if (!userDetail?.name) {
+      setOpenDialog(true);
+      return;
+    }
     setMessages({
       role: "user",
       content: input,
@@ -18,44 +26,50 @@ const Hero = () => {
   };
 
   return (
-    <div className="flex flex-col items-center mt-36 xl:mt-42 gap-2">
-      <h2 className="font-bold text-4xl">{Lookup.HERO_HEADING}</h2>
-      <p className="text-gray-400 font-medium">{Lookup.HERO_DESC}</p>
+    <>
+      <div className="flex flex-col items-center mt-36 xl:mt-42 gap-2">
+        <h2 className="font-bold text-4xl">{Lookup.HERO_HEADING}</h2>
+        <p className="text-gray-400 font-medium">{Lookup.HERO_DESC}</p>
 
-      <div
-        className="p-5 border rounded-xl max-w-xl w-full mt-3"
-        style={{ backgroundColor: Colors.BACKGROUND }}
-      >
-        <div className="flex gap-2">
-          <textarea
-            placeholder={Lookup.INPUT_PLACEHOLDER}
-            className="outline-none bg-transparent w-full h-32 max-h-56 resize-none"
-            onChange={(e) => setUserInput(e.target.value)}
-            value={userInput}
-          />
-          {userInput && (
-            <ArrowRight
-              className="bg-blue-500 p-2 h-10 w-10 rounded-md cursor-pointer"
-              onClick={() => onGenerate(userInput)}
+        <div
+          className="p-5 border rounded-xl max-w-xl w-full mt-3"
+          style={{ backgroundColor: Colors.BACKGROUND }}
+        >
+          <div className="flex gap-2">
+            <textarea
+              placeholder={Lookup.INPUT_PLACEHOLDER}
+              className="outline-none bg-transparent w-full h-32 max-h-56 resize-none"
+              onChange={(e) => setUserInput(e.target.value)}
+              value={userInput}
             />
-          )}
+            {userInput && (
+              <ArrowRight
+                className="bg-blue-500 p-2 h-10 w-10 rounded-md cursor-pointer"
+                onClick={() => onGenerate(userInput)}
+              />
+            )}
+          </div>
+          <div>
+            <Link className="h-5 w-5" />
+          </div>
         </div>
-        <div>
-          <Link className="h-5 w-5" />
+        <div className="flex mt-8 flex-wrap max-w-2xl items-center justify-center gap-3">
+          {Lookup.SUGGSTIONS.map((suggestion, index) => (
+            <h2
+              key={index}
+              className="p-1 px-2 border rounded-full text-sm text-gray-400 hover:text-white cursor-pointer"
+              onClick={() => onGenerate(suggestion)}
+            >
+              {suggestion}
+            </h2>
+          ))}
         </div>
+        <SignInDialog
+          openDialog={openDialog}
+          closeDialog={(v) => setOpenDialog(v)}
+        />
       </div>
-      <div className="flex mt-8 flex-wrap max-w-2xl items-center justify-center gap-3">
-        {Lookup.SUGGSTIONS.map((suggestion, index) => (
-          <h2
-            key={index}
-            className="p-1 px-2 border rounded-full text-sm text-gray-400 hover:text-white cursor-pointer"
-            onClick={() => onGenerate(suggestion)}
-          >
-            {suggestion}
-          </h2>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
--- components/theme-provider.jsx
+++ components/theme-provider.jsx
@@ -3,13 +3,21 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { MessagesContext } from "@/context/MessagesContext";
+import { UserDetailContext } from "@/context/UserDetailContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export function ThemeProvider({ children, ...props }) {
   const [messages, setMessages] = React.useState();
-
+  const [userDetail, setUserDetail] = React.useState();
   return (
-    <MessagesContext.Provider value={{ messages, setMessages }}>
-      <NextThemesProvider {...props}>{children}</NextThemesProvider>
-    </MessagesContext.Provider>
+    <GoogleOAuthProvider
+      clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID_KEY}
+    >
+      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+        <MessagesContext.Provider value={{ messages, setMessages }}>
+          <NextThemesProvider {...props}>{children}</NextThemesProvider>
+        </MessagesContext.Provider>
+      </UserDetailContext.Provider>
+    </GoogleOAuthProvider>
   );
 }
